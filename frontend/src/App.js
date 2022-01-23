fa7fcd050a4bb64ee3744ef165eb1ecfd6c3fd5d
singhsduos <ns9628491678@gmail.com>
2022-01-23 21:05:54 +0530

    files created

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from "./pages/home/Home.js";
+import "./App.css";
+
+
+export default function App() {
+
+    return (
+        <>
+           <Home/>
+        </>
+    );
+}
---
+++ frontend/src/pages/home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./SCSS/Home/Home.css";
+
+const Home = () => {
+    return <div>
+    </div>;
+};
+
+export default Home;
