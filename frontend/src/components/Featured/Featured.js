29ef9fc9cb7ca8ce94cd99fc8503099560953333
singhsduos <ns9628491678@gmail.com>
2022-01-23 23:52:53 +0530

    featuredComponentAdded

---
+++ frontend/src/components/Featured/Featured.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { InfoOutlined, PlayArrow } from "@material-ui/icons";
+import { Button} from "@material-ui/core";
+import "./SCSS/Featured.css";
+
+const Featured = ({type}) => {
+    return (
+        <div className='featured'>
+            
+            {type && (
+                <div className="category">
+                    <span>{type === "movie" ? "Movies" : "Series"}</span>
+                    <select name="genre" id="genre">
+                        <option>Genre</option>
+                        <option value="adventure">Adventure</option>
+                        <option value="comedy">Comedy</option>
+                        <option value="crime">Crime</option>
+                        <option value="fantasy">Fantasy</option>
+                        <option value="historical">Historical</option>
+                        <option value="horror">Horror</option>
+                        <option value="romance">Romance</option>
+                        <option value="sci-fi">Sci-fi</option>
+                        <option value="thriller">Thriller</option>
+                        <option value="western">Western</option>
+                        <option value="animation">Animation</option>
+                        <option value="drama">Drama</option>
+                        <option value="documentary">Documentary</option>
+                    </select>
+                </div>
+            )}
+            <img
+                src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+            />
+            <div className="info">
+                
+
+                <span className='desc'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Aperiam ad asperiores minus. Unde quas dolorem quisquam qui voluptatibus,
+                    quam aliquam deleniti culpa suscipit maiores?
+                </span>
+                <div className="buttons">
+                    <Button className='play'>
+                        <PlayArrow />
+                        <span>Play</span>
+                    </Button>
+                    <Button className='more'>
+                        <InfoOutlined />
+                        <span>Info</span>
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,86 +1,15 @@
 import React from 'react';
+import Featured from '../../components/Featured/Featured';
 import Navbar from '../../components/Navbar/Navbar';
 import "./SCSS/Home/Home.css";
 
 const Home = () => {
-    return <div className='home'>
-        <Navbar />
-        
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla in, ducimus assumenda voluptates nam repellendus. Libero.
-    </div>;
+    return (
+        <div className='home'>
+            <Navbar />
+            <Featured/>
+        </div>
+    );
 };
 
 export default Home;
