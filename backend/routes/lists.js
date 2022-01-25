655f5c43b040a6d69cf0b8d2d40161d2f629b374
singhsduos <ns9628491678@gmail.com>
2022-01-25 17:57:44 +0530

    listAPI-created

--- backend/routes/lists.js
+++ backend/routes/lists.js
@@ -3,5 +3,73 @@ const List = require("../models/listModel");
 const verify = require("../verifyToken");
 
 
+// Create
+router.post("/", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        const newList = new List(req.body);
+
+        try {
+            const savedList = await newList.save();
+            res.status(201).json(savedList);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+    else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+
+
+// Delete
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            await List.findByIdAndDelete(req.params.id);
+            res.status(200).json("The List has been deleted...");
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+
+
+//GET
+
+router.get("/", verify, async (req, res) => {
+    const typeQuery = req.query.type;
+    const genreQuery = req.query.genre;
+    let list = [];
+
+    try {
+        if (typeQuery) { 
+            if (genreQuery) {
+                list = await List.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery, genre: genreQuery } },
+                ]);
+            }
+        //   if we are in the homepage of series or movies no genre
+            else {
+                list = await List.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery } },
+                ]);
+            }
+        }
+        // No typequery we are in the homepage
+        else {
+            list = await List.aggregate([{ $sample: { size: 10 } }]);
+        }
+
+        res.status(200).json(list);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+ })
 
 module.exports = router;
