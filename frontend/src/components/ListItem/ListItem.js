a1683341df1484c8f4bc1f7472e3fab3b046f137
singhsduos <ns9628491678@gmail.com>
2022-01-28 20:21:25 +0530

    build-production

--- frontend/src/components/ListItem/ListItem.js
+++ frontend/src/components/ListItem/ListItem.js
@@ -16,7 +16,7 @@ const ListItem = ({ index, item }) => {
     useEffect(() => {
         const getMovie = async () => {
             try {
-                const res = await axios.get("api/movies/find/" + item, {
+                const res = await axios.get("https://neelesh-netflix.herokuapp.com/api/movies/find/" + item, {
                     headers: {
                         token:
                             "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
