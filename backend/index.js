c3ada95889035384e02452b3a902d774e637e530
singhsduos <ns9628491678@gmail.com>
2022-01-28 21:33:10 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,20 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
 const listRoute = require("./routes/lists");
-
-
+const cors = require("cors");
 dotenv.config();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
+
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
+
+
 
 mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
