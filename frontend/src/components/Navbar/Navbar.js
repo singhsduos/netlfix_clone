78a8cefd9ca8b123733adc1f0b3bfc1e66113284
singhsduos <ns9628491678@gmail.com>
2022-01-29 19:58:08 +0530

    css-update

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -47,9 +47,9 @@ const Navbar = () => {
                 </div>
 
                 <div className="right">
-                    <Search className='icon' />
-                    <span>KID</span>
-                    <Notifications className='icon' />
+                    <Search className='icon navbarmainLinks' />
+                    <span className='span'>KID</span>
+                    <Notifications className='icon span' />
                     <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png"
                         alt="user-profile-pic"
                     />
